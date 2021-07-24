157487a4a00dd238f77bee2c123fb23442ca742b
Dhruv Weaver <dhruv.weaver@gmail.com>
2021-07-24 12:14:55 -0400

    Added loading indicator Main.storyboard: added loading indicator view ViewController.swift: added loading indicator functionaliry

--- MusicLinkTest/ViewController.swift
+++ MusicLinkTest/ViewController.swift
@@ -11,12 +11,14 @@ class ViewController: UIViewController {
     
     @IBOutlet var inputTextField: UITextField!
     @IBOutlet var outputTextField: UITextField!
+    @IBOutlet var loadingIndicator: UIActivityIndicatorView!
     
     var song = Song(link: "")
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        loadingIndicator.hidesWhenStopped = true
     }
     // Sample link: https://music.youtube.com/watch?v=r2CaT3stL7U&feature=share
     @IBAction func translateButtonPressed(_ sender: Any) {
@@ -26,9 +28,12 @@ class ViewController: UIViewController {
             song.link = ""
         }
         
+//        loadingIndicator.isHidden = false
+        loadingIndicator.startAnimating()
         song.getData { json in
             DispatchQueue.main.async {
                 self.outputTextField.text = json.linksByPlatform.spotify.url
+                self.loadingIndicator.stopAnimating()
             }
         }
     }
