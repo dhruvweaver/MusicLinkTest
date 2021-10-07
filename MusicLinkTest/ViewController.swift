6f434814cb949e27d3e11107ebf4eadcf6bde63e
Dhruv Weaver <dhruv.weaver@gmail.com>
2021-10-07 13:05:52 -0400

    ViewController.swift: added tap outside of keyboard gesture to dismiss keyboard

--- MusicLinkTest/ViewController.swift
+++ MusicLinkTest/ViewController.swift
@@ -19,6 +19,15 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         loadingIndicator.hidesWhenStopped = true
+        
+        // allows user to tap outside of keyboard to dismiss
+        let tap = UITapGestureRecognizer(target: self, action: #selector(UIInputViewController.dismissKeyboard))
+        view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+        //Causes the view (or one of its embedded text fields) to resign the first responder status.
+        view.endEditing(true)
     }
     
     @IBAction func clearInputButtonPressed(_ sender: Any) {
